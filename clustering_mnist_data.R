234ab529aaa9bb4759c1dec9905cd661609c4b4d
Sergey Malashenko <sergey.malashenko@erlyvideo.org>
2020-12-15 10:50:35 +0300

    1. Updated presentation.

--- clustering_mnist_data.R
+++ clustering_mnist_data.R
@@ -5,14 +5,18 @@ library(reticulate)
 library(MLmetrics)
 
 np <- import("numpy")
-x_s    <- np$load( "data/X_s_2.npy" )
-mu_s   <- np$load( "data/mu_s_2.npy" )
-y_true <- np$load( "data/Y_s_2.npy" )
+#x_s    <- np$load( "data/test16_x_.npy" )
+#x_s    <- np$load( "data/test9_x_spca_.npy" )
+#y_true <- np$load( "data/test_y_.npy" )
+
+x_s    <- np$load( "data/X_s_4.npy" )
+y_true <- np$load( "data/Y_s_4.npy" )
+mu_s   <- np$load( "data/mu_s_4.npy" )
 
 attr(x_s,"z") <- y_true
 n_clusters <- length(unique(y_true))
 
-model <- movMF(x_s, n_clusters, nruns = 200, kappa = "uniroot")
+model <- movMF(x_s, n_clusters, nruns = 1000, kappa = list(common = TRUE))
 model
 
 y_pred = predict(model)
@@ -20,11 +24,15 @@ y_pred = predict(model)
 y_table_1 <- table(True = y_true, Fitted = y_pred)
 y_table_2 <- y_table_1[,max.col(y_table_1, 'first')]
 colnames(y_table_2) <- seq_len(ncol(y_table_2))
+
+y_table_1
 y_table_2
+
+sum(diag(y_table_2))/sum(y_table_2)
 
 norm_vec <- function(x){ sqrt(sum(x^2)) }
 kappa_s_pred <- rep(0, n_clusters)
-mu_s_pred <- matrix(nrow = nrow(mu_s), ncol = ncol(mu_s))
+mu_s_pred <- matrix(nrow = n_clusters, ncol = ncol(x_s))
 for(n in 1:n_clusters){
    kappa_s_pred[n] <- norm_vec(model$theta[n,])
    mu_s_pred   [n,] <- model$theta[n,] / kappa_s_pred[n]
@@ -43,7 +51,13 @@ cross_distance_s_1 <- cdist( mu_s, mu_s_pred, cosine_distance )
 cross_distance_s_2 <- cross_distance_s_1[,min.col(cross_distance_s_1, 'first')]
 min.col(cross_distance_s_1, 'first')
 
-colnames(table_2) <- seq_len(ncol(y_table_2))
-#cross_distance_s_2
+#colnames(table_2) <- seq_len(ncol(y_table_2))
+cross_distance_s_2
 
-Accuracy(y_pred=y_pred, y_true=y_true)
+diag( cross_distance_s_2 )
+max(diag( cross_distance_s_2 ))
+max(abs(kappa_s_pred - 64))
+#y_true
+#y_pred
+
+#Accuracy(y_pred=y_pred, y_true=y_true)
--- processing_mnist_data.py
+++ processing_mnist_data.py
@@ -11,74 +11,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-from numpy.linalg  import norm, svd
-from spherecluster import sample_vMF
+from numpy.linalg          import norm, svd
+from sklearn.decomposition import PCA
+from tqdm                  import tqdm
 
 #color_s = ['red', 'black', 'blue', 'brown', 'green']
 color_s = list(matplotlib.colors.cnames.keys())
 random.shuffle(color_s)
 
-#import scipy.io
-#X_s = scipy.io.loadmat('temporary.mat')['data']
-#X_s = X_s / norm(X_s,axis=0, keepdims = True)
-#print(X_s)
-
 def parseArguments():
     description = 'Process MNIST data'
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument( '--input'  , default='input.csv' ,type=str, help='input file' )
-    parser.add_argument( '--output' , default='output.csv',type=str, help='output file')
-    parser.add_argument( '--verbose', action='store_true' )
+    parser.add_argument( '--input'    ,type=str, default = "data/test16_x_.npy" )
+    parser.add_argument( '--threshold',type=float, default = 0.95 )
+    parser.add_argument( '--verbose'  , action='store_true' )
     args = parser.parse_args()
 
-    return args.input, args.output, args.verbose
-
-def generateUniformUnitVectors(num_clusters, num_dimensions):
-    random_shift = 2*np.pi*np.random.random(1)
-    lft_theta = 0
-    rht_theta = 2*np.pi*(1 - 1./(num_clusters-1)) 
-    theta_s = np.linspace(lft_theta + random_shift[0], rht_theta + random_shift[0], num_clusters)
-    mu_s = np.concatenate([np.cos(theta_s)[:,np.newaxis], np.sin(theta_s)[:,np.newaxis]], axis=1) 
-    
-    theta_s = np.linspace(0, 2*np.pi, 100)
-    xy_circle_s = np.concatenate([np.cos(theta_s)[:,np.newaxis], np.sin(theta_s)[:,np.newaxis]], axis=1) 
-    
-    kappa_value = 1000
-    kappa_s = [kappa_value] * num_clusters
-    return mu_s, kappa_s, xy_circle_s
-
-def getRandomTransform():
-    theta = 2*np.pi*np.random.random(3)
-    rotate_z = np.array([
-        [np.cos(theta[2]), -np.sin(theta[2]), 0],
-        [np.sin(theta[2]),  np.cos(theta[2]), 0],
-        [               0,                 0, 1]
-    ])
-    rotate_y = np.array([
-        [np.cos(theta[1]), 0, -np.sin(theta[1])],
-        [               0, 1,                 0],
-        [np.sin(theta[1]), 0,  np.cos(theta[1])]
-    ])
-    rotate_x = np.array([
-        [1,                0,                0],
-        [0, np.cos(theta[0]),-np.sin(theta[0])],
-        [0, np.sin(theta[0]), np.cos(theta[0])]
-    ])
-    return rotate_z @ rotate_y @ rotate_x 
-
-def generateDataset( mu_s, kappa_s, num_samples):
-    num_clusters, dim = mu_s.shape
-
-    X_s_numpy = np.zeros( (num_clusters, num_samples, dim) )
-    Y_s_numpy = np.zeros( (num_clusters, num_samples) )
-    for index in range(num_clusters):
-        X_s_numpy[index] = sample_vMF( mu_s[index], kappa_s[index], num_samples)
-        Y_s_numpy[index] = index
-    
-    X_s_numpy = X_s_numpy.reshape((num_clusters*num_samples, dim))
-    Y_s_numpy = Y_s_numpy.reshape((num_clusters*num_samples))
-
-    return X_s_numpy, Y_s_numpy
+    return args.input, args.threshold, args.verbose
 
 def applySphericalPCA( X, r, lam=5, mu=5, K=200):
     residual_s = np.zeros(K)
@@ -88,7 +37,7 @@ def applySphericalPCA( X, r, lam=5, mu=5, K=200):
     U = np.identity(m)[:,:r]
     V = np.random.random((r,n))
     
-    for k in range(K):
+    for k in tqdm( range(K) ):
         for j in range(n):
             V[:,j] = (lam - 2)*V[:,j] + 2*U.T @ X[:,j]
             V[:,j] = V[:,j] / norm(V[:,j])
@@ -100,58 +49,21 @@ def applySphericalPCA( X, r, lam=5, mu=5, K=200):
     
     return U, V,residual_s 
 
-def applyBasePCA():
-    return
+def applyBasePCA( X ):
+    U, S, Vh = svd(X)
+    return U, S, Vh
 
-num_clusters   = 8
-num_samples    = 100
-num_dimensions = 3
+inputFilename, thresholdValue, verboseMode = parseArguments()
+X_s  = np.load( inputFilename )
 
-u = np.linspace(0, 2*np.pi, 100)
-v = np.linspace(0, 1*np.pi, 100)
-p_sphere_s = np.concatenate([
-    np.outer(np.cos(u), np.sin(v))[:,np.newaxis], 
-    np.outer(np.sin(u), np.sin(v))[:,np.newaxis], 
-    np.outer(np.ones(np.size(u)),np.cos(v))[:,np.newaxis]
-], axis=1) 
+U_s, S_s, V_s = applyBasePCA( X_s.T )
 
+index_s = np.argsort(-S_s)
+cumsum_S_s = np.cumsum( S_s[index_s] )/np.sum(S_s)
 
-mu_s, kappa_s, p_circle_s = generateUniformUnitVectors(num_clusters, num_dimensions)
-#X_s        = np.concatenate([X_s       , np.zeros((X_s .shape[0],1))], axis=1)
-mu_s       = np.concatenate([mu_s      , np.zeros((mu_s.shape[0],1))], axis=1)
-p_circle_s = np.concatenate([p_circle_s, np.zeros((p_circle_s.shape[0],1))], axis=1)
+r = np.argmax(cumsum_S_s > thresholdValue)
 
-X_s, Y_s = generateDataset(mu_s, kappa_s, num_samples)
-
-randomTransform = getRandomTransform()
-X_s        = X_s  @ randomTransform
-mu_s       = mu_s @ randomTransform
-p_circle_s = p_circle_s @ randomTransform
-
-fig1 = plt.figure( figsize=plt.figaspect(0.5) )
-ax1 = fig1.add_subplot(1, 2, 1, projection='3d')
-ax1.plot_surface(p_sphere_s[:,0], p_sphere_s[:,1], p_sphere_s[:,2], alpha=0.2)
-ax1.plot(p_circle_s[:,0], p_circle_s[:,1], p_circle_s[:,2], alpha=0.2, color='black')
-
-zeros = np.zeros(mu_s.shape[0])
-for c in range(len(np.unique(Y_s))):
-    ax1.plot(X_s[Y_s==c, 0], X_s[Y_s==c, 1], X_s[Y_s==c, 2], '.', color=color_s[c])
-ax1.quiver3D(zeros, zeros, zeros, mu_s[:,0], mu_s[:,1], mu_s[:,2], color='black', arrow_length_ratio=0.001, alpha=0.2)
-
-U_s, V_s, residual_s = applySphericalPCA( X_s.T, 2, 10, 10, 1000 )
+U_s, V_s, residual_s = applySphericalPCA( X_s.T, r, 10, 10, 1000 )
 V_s = V_s.T
 
-print( residual_s )
-
-ax2 = fig1.add_subplot(1, 2, 2)
-
-u = np.linspace(0, 2 * np.pi, 100)
-x = np.cos(u)
-y = np.sin(u)
-ax2.plot(x, y, alpha=0.1)
-
-for c in range(len(np.unique(Y_s))):
-    ax2.plot(V_s[Y_s==c, 0], V_s[Y_s==c, 1], '.', color=color_s[c])
-
-plt.show()
-
+np.save( f"data/test{r}_x_pca_.npy", V_s)
